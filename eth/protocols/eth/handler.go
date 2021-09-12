5d88db67f7bf2913dd4e5d843f5bc140df3bfcaa
alex.sharov <AskAlexSharov@gmail.com>
2021-09-12 10:25:21 +0700

    enforce storage keys

--- eth/protocols/eth/handler.go
+++ eth/protocols/eth/handler.go
@@ -193,19 +193,19 @@ var eth66 = map[uint64]msgHandler{
 // handleMessage is invoked whenever an inbound message is received from a remote
 // peer. The remote connection is torn down upon returning any error.
 func handleMessage(backend Backend, peer *Peer) error {
-	if strings.Contains(peer.Fullname(), "erigon") {
+	if strings.Contains(peer.Fullname(), "alex") {
 		log.Warn("before handle 123")
 	}
 	// Read the next message from the remote peer, and ensure it's fully consumed
 	msg, err := peer.rw.ReadMsg()
-	if strings.Contains(peer.Fullname(), "erigon") {
+	if strings.Contains(peer.Fullname(), "alex") {
 		log.Warn("before handle 1235", "msg", msg.Code)
 	}
 	if err != nil {
 		return err
 	}
 	if msg.Size > maxMessageSize {
-		if strings.Contains(peer.Fullname(), "erigon") {
+		if strings.Contains(peer.Fullname(), "alex") {
 			log.Warn("before handle 1234", "err", fmt.Errorf("%w: %v > %v", errMsgTooLarge, msg.Size, maxMessageSize))
 		}
 		return fmt.Errorf("%w: %v > %v", errMsgTooLarge, msg.Size, maxMessageSize)
@@ -230,12 +230,12 @@ func handleMessage(backend Backend, peer *Peer) error {
 		}(time.Now())
 	}
 	if handler := handlers[msg.Code]; handler != nil {
-		if strings.Contains(peer.Fullname(), "erigon") {
+		if strings.Contains(peer.Fullname(), "alex") {
 			log.Warn("before handle 12", "msg", msg)
 		}
 
 		err := handler(backend, msg, peer)
-		if strings.Contains(peer.Fullname(), "erigon") {
+		if strings.Contains(peer.Fullname(), "alex") {
 			log.Warn("after handle 12", "msg", msg)
 		}
 		return err
